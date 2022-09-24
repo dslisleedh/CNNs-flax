3d4c179f9882ceebc935c2bf87e9e138b1990dff
dslisleedh <gnova6kud@gmail.com>
2022-09-24 22:20:53 +0900

    add GoogLeNet, VGGNet, ResNet, IdentityMappingResNet, DenseNet

---
+++ src/layers.py
@@ -0,0 +1,164 @@
+import jax
+from jax import lax
+import jax.numpy as jnp
+import numpy as np
+import flax
+import flax.linen as nn
+
+from typing import Sequence, Tuple, Union, Optional
+
+
+class LocalResponsibleNormalization(nn.Module):
+    k: int = 2
+    alpha: float = 1.
+    beta: float = .75
+    across_channel: bool = True
+
+    @nn.compact
+    def __call__(self, x):
+        div = x.pow(2)
+        if self.across_channel:
+            div = div.expand_dims(1)
+            div = nn.avg_pool(div, window_shape=(self.k, 1, 1), strides=(1, 1, 1), padding=(int(self.k / 2), 0, 0))
+            div = div.squeeze(1)
+
+        else:
+            div = nn.avg_pool(div, window_shape=(self.k, self.k), strides=(1, 1), padding="SAME")
+        div = jnp.power(div * self.alpha + 1., self.beta)
+
+        return x / div
+
+
+class Inception(nn.Module):
+    conv_1_filters: int
+    conv_3_filters: Sequence[int]
+    conv_5_filters: Sequence[int]
+    max_pool_filters: int
+    down_sample: bool = False
+    act = nn.relu
+
+    @nn.compact
+    def __call__(self, x):
+        if self.downsamle:
+            x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+
+        conv_1 = nn.Conv(self.conv_1_filters, (1, 1), padding="SAME")(x)
+        conv_1 = self.act(conv_1)
+
+        conv_3 = nn.Conv(self.conv_3_filters[0], (1, 1), padding="SAME")(x)
+        conv_3 = self.act(conv_3)
+        conv_3 = nn.Conv(self.conv_3_filters[1], (3, 3), padding="SAME")(conv_3)
+        conv_3 = self.act(conv_3)
+
+        conv_5 = nn.Conv(self.conv_5_filters[0], (1, 1), padding="SAME")(x)
+        conv_5 = self.act(conv_5)
+        conv_5 = nn.Conv(self.conv_5_filters[1], (5, 5), padding="SAME")(conv_5)
+        conv_5 = self.act(conv_5)
+
+        max_pool = nn.max_pool(x, window_shape=(3, 3), strides=(1, 1), padding="SAME")
+        max_pool = nn.Conv(self.max_pool_filters, (1, 1), padding="SAME")(max_pool)
+        max_pool = self.act(max_pool)
+
+        return jnp.concatenate([conv_1, conv_3, conv_5, max_pool], axis=-1)
+
+
+class ResBlock(nn.Module):
+    n_filters: Sequence[int]
+    act = nn.relu
+    down_sample: bool = False
+    increase_dim: bool = False
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        if self.down_sample:
+            skip = nn.Conv(self.n_filters[-1], (3, 3), strides=(2, 2), padding="SAME")(x)
+            x = nn.Conv(
+                self.n_filters[0], (3, 3) if len(self.n_filters) == 2 else (1, 1), strides=(2, 2),
+                padding="SAME", use_bias=False
+            )(x)
+        else:
+            if self.increase_dim:
+                skip = nn.Conv(self.n_filters[-1], (1, 1), padding="SAME")(x)
+            else:
+                skip = jnp.identity(x)
+            x = nn.Conv(
+                self.n_filters[0], (3, 3) if len(self.n_filters) == 2 else (1, 1),
+                padding="SAME", use_bias=False
+            )(x)
+        x = nn.BatchNorm()(x, use_running_average=not training)
+        for i, n_filter in enumerate(self.n_filters[1:]):
+            x = self.act(x)
+            x = nn.Conv(n_filter, (3, 3) if i == 0 else (1, 1), padding="SAME", use_bias=False)(x)
+            x = nn.BatchNorm()(x, use_running_average=not training)
+        x += skip
+        x = self.act(x)
+        return x
+
+
+class IdentityResBlock(nn.Module):
+    n_filters: Sequence[int]
+    act = nn.relu
+    down_sample: bool = False
+    increase_dim: bool = False
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        if self.down_sample:
+            skip = nn.Conv(self.n_filters[-1], (1, 1), strides=(2, 2), padding="SAME")(x)
+            x = nn.BatchNorm()(x, use_running_average=not training)
+            x = self.act(x)
+            x = nn.Conv(
+                self.n_filters[0], (3, 3) if len(self.n_filters == 2) else (1, 1), strides=(2, 2),
+                padding="SAME", use_bias=False
+            )(x)
+        else:
+            if self.increase_dim:
+                skip = nn.Conv(self.n_filters[-1], (1, 1), padding="SAME")(x)
+            else:
+                skip = jnp.identity(x)
+            x = nn.BatchNorm()(x, use_running_average=not training)
+            x = self.act(x)
+            x = nn.Conv(
+                self.n_filters[0], (3, 3) if len(self.n_filters == 2) else (1, 1), padding="SAME"
+            )(x)
+        x = nn.BatchNorm()(x, use_running_average=not training)
+        for i, n_filter in enumerate(self.n_filters[1:]):
+            x = self.act(x)
+            x = nn.Conv(self.n_filters, (3, 3) if i == 0 else (1, 1), padding="SAME")(x)
+            x = nn.BatchNorm()(x, use_running_average=not training)
+        x = x + skip
+        return x
+
+
+class DenseLayer(nn.Module):
+    n_filters: int
+    act = nn.relu
+    dropout_rate: float = .2
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        y = nn.BatchNorm()(x, use_running_average=not training)
+        y = self.act(y)
+        y = nn.Conv(self.n_filters * 4, (1, 1), padding="VALID")(y)
+        y = nn.Dropout(self.dropout_rate)(y, deterministic=not training)
+        y = nn.BatchNorm()(y, use_running_average=not training)
+        y = self.act(y)
+        y = nn.Conv(self.n_filters, (3, 3), padding="SAME")(y)
+        y = nn.Dropout(self.dropout_rate)(y, deterministic=not training)
+        return jnp.concatenate([x, y], axis=-1)
+
+
+class DenseTransitionLayer(nn.Module):
+    n_filters: int
+    act = nn.relu
+    dropout_rate: float = .2
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        y = nn.BatchNorm()(x, use_running_average=not training)
+        y = self.act(y)
+        y = nn.Conv(self.n_filters, (1, 1), padding="VALID")(y)
+        y = nn.Dropout(self.dropout_rate)(y, deterministic=not training)
+        y = nn.avg_pool(y, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+        return y
+
---
+++ src/models.py
@@ -0,0 +1,183 @@
+import jax
+from jax import lax
+import jax.numpy as jnp
+import numpy as np
+import flax
+import flax.linen as nn
+
+from src.layers import *
+
+from typing import Sequence, Tuple, Union
+
+
+class GoogLeNet(nn.Module):
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        x = nn.Conv(64, (7, 7), strides=(2, 2), padding="SAME")(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+        x = LocalResponsibleNormalization()(x)
+        x = nn.Conv(64, (1, 1), padding="SAME")(x)
+        x = nn.relu(x)
+        x = nn.Conv(192, (3, 3), padding="SAME")(x)
+        x = nn.relu(x)
+        x = LocalResponsibleNormalization()(x)
+
+        x = Inception(64, (94, 128), (16, 32), 32, downsample=True)(x)      # inception_3a
+        x = Inception(128, (128, 192), (32, 96), 64)(x)                     # inception_3b
+
+        o_1 = Inception(192, (96, 208), (16, 48), 64, downsample=True)(x)   # inception_4a
+        x = Inception(160, (112, 224), (24, 64), 64)(o_1)                   # inception_4b
+        x = Inception(128, (128, 256), (24, 64), 64)(x)                     # inception_4c
+        o_2 = Inception(112, (144, 288), (32, 64), 64)(x)                   # inception_4d
+        x = Inception(256, (160, 320), (32, 128), 128)(o_2)                 # inception_5e
+
+        x = Inception(256, (160, 320), (32, 128), 128)(x, downsample=True)  # inception_5a
+        x = Inception(348, (192, 348), (48, 128), 128)(x)                   # inception_5b
+
+        # Classification head
+        x = nn.avg_pool(x, window_shape=(7, 7), strides=(1, 1), padding="VALID")
+        x = x.flatten()
+        x = nn.Dropout(rate=0.4)(x, deterministic=not training)
+        x = nn.Dense(1000)(x)
+        y = nn.softmax(x)
+
+        if training:
+            # Auxiliary classifier 1
+            o_1 = nn.avg_pool(o_1, window_shape=(5, 5), strides=(3, 3), padding="VALID")
+            o_1 = o_1.flatten()
+            o_1 = nn.Dense(1024)(o_1)
+            o_1 = nn.relu(o_1)
+            o_1 = nn.Dropout(rate=0.7)(o_1, deterministic=False)
+            o_1 = nn.Dense(1000)(o_1)
+            o_1 = nn.softmax(o_1)
+
+            # Auxiliary classifier 2
+            o_2 = nn.avg_pool(o_2, window_shape=(5, 5), strides=(3, 3), padding="VALID")
+            o_2 = o_2.flatten()
+            o_2 = nn.Dense(1024)(o_2)
+            o_2 = nn.relu(o_2)
+            o_2 = nn.Dropout(rate=0.7)(o_2, deterministic=False)
+            o_2 = nn.Dense(1000)(o_2)
+            o_2 = nn.softmax(o_2)
+
+            return y, o_1, o_2
+
+        else:
+            return y
+
+
+class VGGNet(nn.Module):
+    confing: dict
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        n_filters = self.config["n_filters"]
+        # Feature extraction
+        for i, n_layers in enumerate(self.config['n_layers']):
+            for _ in range(n_layers):
+                x = nn.Conv(n_filters, (3, 3), padding="SAME")(x)
+                x = nn.relu(x)
+            if i == 0 and self.config["use_lrn"]:
+                x = LocalResponsibleNormalization()(x)
+            x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2), padding="VALID")
+            n_filters *= 2
+
+        # Classification head
+        x = x.flatten()
+        x = nn.Dropout(rate=0.5)(x, deterministic=not training)
+        x = nn.Dense(4096)(x)
+        x = nn.relu(x)
+        x = nn.Dropout(rate=0.5)(x, deterministic=not training)
+        x = nn.Dense(4096)(x)
+        x = nn.relu(x)
+        x = nn.Dense(1000)(x)
+        y = nn.softmax(x)
+        return y
+
+
+class ResNet(nn.Module):
+    config: dict
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        # Intro
+        x = nn.Conv(self.config['intro']['n_filters'], (7, 7), strides=(2, 2), padding="SAME")(x)
+        x = nn.relu(x)
+
+        # Feature extraction
+        for i, n_blocks in enumerate(len(self.config['n_filters'])):
+            if i == 0:
+                x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+                x = ResBlock(self.config['n_filters'][i], increase_dim=True)(x, training=training)
+            else:
+                x = ResBlock(self.config['n_filters'][i], downsample=True)(x, training=training)
+            for _ in range(n_blocks - 1):
+                x = ResBlock(self.config['n_filters'][i])(x, training=training)
+
+        # Classification head
+        x = nn.avg_pool(x, window_shape=(7, 7), strides=(1, 1), padding="VALID")
+        x = x.flatten()
+        x = nn.Dense(1000)(x)
+        y = nn.softmax(x)
+        return y
+
+
+class IdentityResNet(nn.Module):
+    config: dict
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        # Intro
+        x = nn.Conv(self.config['intro']['n_filters'], (7, 7), strides=(2, 2), padding="SAME")(x)
+        x = nn.relu(x)
+
+        # Feature extraction
+        for i, n_blocks in enumerate(len(self.config['n_filters'])):
+            if i == 0:
+                x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+                x = IdentityResBlock(self.config['n_filters'][i], increase_dim=True)(x, training=training)
+            else:
+                x = IdentityResBlock(self.config['n_filters'][i], downsample=True)(x, training=training)
+            for _ in range(n_blocks - 1):
+                x = IdentityResBlock(self.config['n_filters'][i])(x, training=training)
+
+        # Classification head
+        x = nn.avg_pool(x, window_shape=(7, 7), strides=(1, 1), padding="VALID")
+        x = x.flatten()
+        x = nn.Dense(1000)(x)
+        y = nn.softmax(x)
+        return y
+
+
+class DenseNet(nn.Module):
+    config: dict
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        n_filters = self.config['initial_filters']
+        growth_rate = self.config['growth_rate']
+        # Intro
+        x = nn.Conv(n_filters, (7, 7), strides=(2, 2), padding="SAME")(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+
+        # Feature extraction
+        for i, n_blocks in enumerate(self.config['n_blocks']):
+            x_list = [x]
+            for _ in range(n_blocks):
+                n_filters += growth_rate
+                x = DenseLayer(n_filters)(jnp.concatenate(x_list, axis=-1), training=training)
+                x_list.append(x)
+            n_filters = int(n_filters * self.config['theta'])
+            x = jnp.concatenate(x_list, axis=-1)
+            if i != len(self.config['n_blocks']) - 1:
+                x = DenseTransitionLayer(n_filters)(x, training=training)
+
+        # Classification head
+        x = nn.avg_pool(x, window_shape=(7, 7), strides=(1, 1), padding="VALID")
+        x = x.flatten()
+        x = nn.Dense(1000)(x)
+        y = nn.softmax(x)
+        return y
