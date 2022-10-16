78dd04a6ebb5ee4c375d8c0dc1e1a84a84199cb3
dslisleedh <gnova6kud@gmail.com>
2022-10-16 11:23:46 +0900

    add DynamicConv2D Layer

--- src/layers.py
+++ src/layers.py
@@ -4,6 +4,7 @@ import jax.numpy as jnp
 import numpy as np
 import flax
 import flax.linen as nn
+import einops
 
 from typing import Sequence, Tuple, Union, Optional
 
@@ -254,3 +255,98 @@ class SESkipBlock(nn.Module):
         attention = nn.sigmoid(attention)
 
         return y * attention + skip
+
+
+class DynamicConv2D(nn.Module):
+    n_filters: int
+    kernel_size: Union[int, Sequence[int]] = (3, 3)
+    strides: Union[int, Sequence[int]] = (1, 1)
+    padding: str = 'SAME'
+    feature_group_count: int = 1
+    use_bias: bool = True
+    act = staticmethod(nn.relu)
+
+    r_filters: int = 4
+    n_kernels: int = 4
+    initial_temperature: float = 30.
+    temperature_decay: float = 1e-4
+
+    @nn.compact
+    def __call__(self, x, training: bool):
+        input_batches, _, _, input_filters = x.shape
+
+        if type(self.kernel_size) == int:
+            kernel_size = (self.kernel_size,) * 2
+        else:
+            kernel_size = self.kernel_size
+
+        if type(self.strides) == int:
+            strides = (self.strides,) * 2
+        else:
+            strides = self.strides
+
+        reduction_kernel = self.param(
+            'reduction_kernel', nn.initializers.variance_scaling(.02, mode='fan_in', distribution='truncated_normal'),
+            (input_filters, self.r_filters)
+        )
+        attention_kernel = self.param(
+            'attention_kernel', nn.initializers.variance_scaling(.02, mode='fan_in', distribution='truncated_normal'),
+            (self.r_filters, self.n_kernels)
+        )
+        kernels = self.param(
+            'conv_kernels', nn.initializers.variance_scaling(.02, mode='fan_in', distribution='truncated_normal'),
+            (1, self.n_kernels,) + kernel_size + (input_filters, self.n_filters,)
+        )
+        temperature = self.variable(
+            'state', 'temperature', lambda s: jnp.ones(s, jnp.float32) * 30, (1,)
+        )
+
+        pool = jnp.mean(x, axis=(1, 2))
+        pool_reduction = self.act(
+            lax.dot_general(pool, reduction_kernel, (((1,), (0,)), ((), ())))
+        )
+        attention = nn.softmax(
+            lax.dot_general(pool_reduction, attention_kernel, (((1,), (0,)), ((), ()))) / temperature.value
+        )
+        attention = attention[:, :, jnp.newaxis, jnp.newaxis, jnp.newaxis, jnp.newaxis]
+        w_tiled = jnp.sum(
+            attention * kernels, axis=1
+        )
+        w_tiled = einops.rearrange(
+            w_tiled, 'B H W F_in F_out -> H W F_in (B F_out)'
+        )
+        x = jnp.expand_dims(
+            einops.rearrange(
+                x, 'B H W C -> H W (B C)'
+            ), axis=0
+        )
+        y = lax.conv_general_dilated(
+            x, w_tiled, window_strides=strides, padding=self.padding, dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
+            feature_group_count=input_batches
+        )
+        y = einops.rearrange(
+            y[0], 'H W (B C) -> B H W C', B=input_batches
+        )
+
+        if self.use_bias:
+            bias = self.param(
+                'bias', nn.initializers.zeros, (1, self.n_kernels, self.n_filters)
+            )
+            attention = jnp.squeeze(
+                attention, axis=[3, 4, 5]
+            )
+            b_tiled = jnp.sum(attention * bias, axis=1)
+            b_tiled = jnp.reshape(
+                b_tiled, (input_batches, 1, 1, self.n_filters)
+            )
+            y = y + b_tiled
+
+        y = nn.BatchNorm()(y, use_running_average=not training)
+        y = self.act(y)
+
+        if training:
+            temperature.value = max(
+                1, temperature.value - self.temperature_decay
+            )
+
+        return y
